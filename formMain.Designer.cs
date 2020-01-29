11f8121d563d5513209d24a0f9af4b2d36fa1fa5
TONG YI CHEN TP <1901037J@student.tp.edu.sg>
2020-01-29 11:30:10 +0800

    Add project files.

---
+++ formMain.Designer.cs
@@ -0,0 +1,152 @@
+﻿namespace Anagram
+{
+    partial class formMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lboxWords = new System.Windows.Forms.ListBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.lblTitle = new System.Windows.Forms.Label();
+            this.tbChars = new System.Windows.Forms.TextBox();
+            this.lblChars = new System.Windows.Forms.Label();
+            this.btnGenerate = new System.Windows.Forms.Button();
+            this.btnRandom = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lboxWords
+            // 
+            this.lboxWords.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lboxWords.FormattingEnabled = true;
+            this.lboxWords.ItemHeight = 25;
+            this.lboxWords.Location = new System.Drawing.Point(6, 158);
+            this.lboxWords.Margin = new System.Windows.Forms.Padding(2);
+            this.lboxWords.Name = "lboxWords";
+            this.lboxWords.Size = new System.Drawing.Size(498, 354);
+            this.lboxWords.TabIndex = 0;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.SkyBlue;
+            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(509, 50);
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // lblTitle
+            // 
+            this.lblTitle.AutoSize = true;
+            this.lblTitle.BackColor = System.Drawing.Color.SkyBlue;
+            this.lblTitle.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTitle.Location = new System.Drawing.Point(12, 9);
+            this.lblTitle.Name = "lblTitle";
+            this.lblTitle.Size = new System.Drawing.Size(261, 37);
+            this.lblTitle.TabIndex = 2;
+            this.lblTitle.Text = "Anagram Solver";
+            // 
+            // tbChars
+            // 
+            this.tbChars.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbChars.Location = new System.Drawing.Point(130, 77);
+            this.tbChars.Name = "tbChars";
+            this.tbChars.Size = new System.Drawing.Size(374, 31);
+            this.tbChars.TabIndex = 3;
+            // 
+            // lblChars
+            // 
+            this.lblChars.AutoSize = true;
+            this.lblChars.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblChars.Location = new System.Drawing.Point(1, 80);
+            this.lblChars.Name = "lblChars";
+            this.lblChars.Size = new System.Drawing.Size(123, 25);
+            this.lblChars.TabIndex = 4;
+            this.lblChars.Text = "Characters:";
+            // 
+            // btnGenerate
+            // 
+            this.btnGenerate.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnGenerate.ForeColor = System.Drawing.Color.Black;
+            this.btnGenerate.Location = new System.Drawing.Point(371, 114);
+            this.btnGenerate.Name = "btnGenerate";
+            this.btnGenerate.Size = new System.Drawing.Size(133, 39);
+            this.btnGenerate.TabIndex = 5;
+            this.btnGenerate.Text = "Generate";
+            this.btnGenerate.UseVisualStyleBackColor = true;
+            this.btnGenerate.Click += new System.EventHandler(this.btnGenerate_Click);
+            // 
+            // btnRandom
+            // 
+            this.btnRandom.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnRandom.ForeColor = System.Drawing.Color.Black;
+            this.btnRandom.Location = new System.Drawing.Point(232, 114);
+            this.btnRandom.Name = "btnRandom";
+            this.btnRandom.Size = new System.Drawing.Size(133, 39);
+            this.btnRandom.TabIndex = 6;
+            this.btnRandom.Text = "Random";
+            this.btnRandom.UseVisualStyleBackColor = true;
+            this.btnRandom.Click += new System.EventHandler(this.btnRandom_Click);
+            // 
+            // formMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(509, 517);
+            this.Controls.Add(this.btnRandom);
+            this.Controls.Add(this.btnGenerate);
+            this.Controls.Add(this.lblChars);
+            this.Controls.Add(this.tbChars);
+            this.Controls.Add(this.lblTitle);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.lboxWords);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "formMain";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Anagram Solver";
+            this.Load += new System.EventHandler(this.formMain_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox lboxWords;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label lblTitle;
+        private System.Windows.Forms.TextBox tbChars;
+        private System.Windows.Forms.Label lblChars;
+        private System.Windows.Forms.Button btnGenerate;
+        private System.Windows.Forms.Button btnRandom;
+    }
+}
+
---
+++ formMain.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Windows.Forms;
+
+namespace Anagram
+{
+    public partial class formMain : Form
+    {
+        Random _rnd = new Random();
+        //dictionary loaded into memory
+        List<string> _dictionary = new List<string>();
+        //list of characters
+        string charString;
+
+        public formMain()
+        {
+            InitializeComponent();
+        }
+
+        private void btnRandom_Click(object sender, EventArgs e)
+        {
+            //randomly generate 10 characters and display into tbChars
+            var generatedLetters = "";
+            for (int i = 0; i < 10; i++)
+            {
+                var rndChar = (char)_rnd.Next('a', 'z');
+                generatedLetters += $",{rndChar}";
+            }
+            tbChars.Text = generatedLetters.TrimStart(',');
+        }
+
+        private void formMain_Load(object sender, EventArgs e)
+        {
+            //load all words into dictionary
+            var filePath = @"C:\Visual Studio Projects\Anagram\words_alpha.txt";
+            StreamReader fileRead = new StreamReader(filePath);
+            while (!fileRead.EndOfStream)
+            {
+                _dictionary.Add(fileRead.ReadLine());
+            }
+        }
+
+        private void btnGenerate_Click(object sender, EventArgs e)
+        {
+            charString = "";
+            var charCount = 0;
+            var _dictionaryF = new List<string>();
+            var charList = new List<char>();
+            //add the characters in textbox into list of characters
+            foreach (var letter in tbChars.Text.Split(','))
+            {
+                charString += letter;
+                charList.Add(letter[0]);
+                charCount++;
+            }
+
+            //filtering down dictionary to not kill computer
+            foreach (var word in _dictionary)
+            {
+                if (word.Length<=charCount && charList.Contains(word[0]))
+                {
+                    _dictionaryF.Add(word);
+                }
+            }
+
+            Console.WriteLine(charString);
+
+            var results = new List<string>();
+
+            findWords(charString, "", results);
+
+            Console.WriteLine("finished recursion");
+
+            var validWords = new List<string>();
+
+            foreach (var anagram in results)
+            {
+                if (_dictionaryF.Contains(anagram) && validWords.Contains(anagram) == false)
+                {
+                    validWords.Add(anagram);
+                }
+            }
+
+            Console.WriteLine("comparing done");
+
+            lboxWords.DataSource = null;
+            lboxWords.DataSource = validWords;
+        }
+
+        private void findWords(string charLeft, string prefix, IList<string> result)
+        {
+            if (charLeft.Length == 0)
+            {
+                result.Add(prefix);
+                return;
+            }
+
+            for (int i = 0; i < charLeft.Length; i++)
+            {
+                //setting first character as prefix
+                var tempPrefix = charLeft[i].ToString();
+                //making string of the rest of the letters
+                var tempCharLeft = charLeft.Substring(0, i) + charLeft.Substring(i + 1);
+                //new list to grab all anagram
+                var tempResult = new List<string>();
+                //recursion to iterate through all possible prefix
+                findWords(tempCharLeft, tempPrefix, tempResult);
+                foreach (var anagram in tempResult)
+                {
+                    result.Add($"{prefix}{anagram}");
+                    Console.WriteLine(prefix + anagram);
+                }
+            }
+        }
+    }
+}
